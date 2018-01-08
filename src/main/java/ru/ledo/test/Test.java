cd30ab6ef7b805a5e7a63079b8daf11cd67177e0
trort <ledo.au@gmail.com>
2018-01-08 18:12:38 +0300

    java 8 studding

--- src/main/java/ru/ledo/test/Test.java
+++ src/main/java/ru/ledo/test/Test.java
@@ -1,10 +1,45 @@
 package ru.ledo.test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        //Happy NEW YEAR!!!//
-    }
+    public static void main (String[] args) {
+        /**
+        Formula testFormula = new FormulaImpl();
 
+        testFormula.calculate(1);
+        testFormula.sqrt(100);
+         */
+
+        Formula test = new Formula() {
+            @Override
+            public double calculate(int a) {
+                return sqrt(a * 100);
+            }
+        };
+        test.calculate(1);
+        test.sqrt(100);
+
+
+        List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+        Collections.sort(names, (o1, o2) -> o1.compareTo(o2));
+
+        //Converter<String, Integer> convertor = (from) -> Integer.valueOf(from);
+
+        Converter<String, Integer> convertor = Integer::valueOf;
+        Integer result = convertor.conver("123");
+        System.out.println("result = " + result);
+
+        PersonFactory<Person> testFactory = Person::new;
+        testFactory.createPerson("Peter", "Jackson");
+
+        int num = 10;
+        Converter<Integer, String> secondConveeter = (from) -> String.valueOf(num + from);
+        secondConveeter.conver(2);
+
+
+    }
 }
